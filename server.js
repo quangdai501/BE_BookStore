5f3fda08606d58e8850f57698d7dbf9233c001ae
quangdai501 <72977901+quangdai501@users.noreply.github.com>
2021-07-15 18:22:07 +0700

    update

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.use(bodyParser.json());
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000',
-        'https://nongsan3ae.netlify.app']
+    origin: '*'
 }));
 // app.use(cors({ credentials: true, origin: "https://nongsan3ae.netlify.app" }));
 app.use(bodyParser.urlencoded({ extended: true }));
