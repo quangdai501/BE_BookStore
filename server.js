b99d746e5696d0a6b4d3f8aa678409fa93d0abc7
quangdai501 <tranquangdait02@gmail.com>
2021-10-30 17:39:49 +0700

    first

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const path = require('path');
-
-const app = express();
-const port = process.env.PORT || 5000;
+// const path = require('path');
 const router = require('./api/route');
 const db = require('./api/config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 
