bd977a2e24c96e29bd3c483bfea91ab27cab6a54
lnadaigia <72127469+lnadaigia@users.noreply.github.com>
2021-11-25 09:48:13 +0700

    Update utils.js

--- api/utils.js
+++ api/utils.js
@@ -1,57 +1,59 @@
 const jwt = require('jsonwebtoken');
 
 const getToken = (user) => {
-  return jwt.sign(
-    {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: '24h',
-    }
-  );
+    return jwt.sign({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        },
+        process.env.JWT_SECRET, {
+            expiresIn: '24h',
+        }
+    );
 };
 
 const isAuth = (req, res, next) => {
-  const authentizationHeaders = req.headers.authentization;
-  const token = authentizationHeaders.split(' ')[1];
-  // const userInfo = JSON.parse(req.cookies.userInfo);
-  // const { token } = userInfo;
-  try {
-    if (token) {
-      jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-        if (err) {
-          return res.status(401).send({ message: 'Invalid Token' });
+
+
+    // const userInfo = JSON.parse(req.cookies.userInfo);
+    // const { token } = userInfo;
+    try {
+        // const authentizationHeaders = req.headers.authentization;
+        const authentizationHeaders = req.headers.authorization;
+        const token = authentizationHeaders.split(' ')[1];
+        if (token) {
+            jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+                if (err) {
+                    return res.status(401).send({ message: 'Invalid Token' });
+                }
+                req.user = decode;
+                next();
+                return;
+            });
+        } else {
+            return res.status(401).send({ message: 'Token is not supplied.' });
         }
-        req.user = decode;
-        next();
-        return;
-      });
-    } else {
-      return res.status(401).send({ message: 'Token is not supplied.' });
+    } catch (error) {
+        res.status(500).send(error.message);
+        // console.log(error.message);
     }
-  } catch (error) {
-    console.log(error.message);
-  }
 };
 
 const isAdmin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res.status(401).send({ message: 'Invalid Admin Token' });
-  }
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(401).send({ message: 'Invalid Admin Token' });
+    }
 }
 
 const isShipper = (req, res, next) => {
-  if (req.user && req.user.role === 'shipper') {
-    next();
-  } else {
-    res.status(401).send({ message: 'Invalid Admin Token' });
-  }
+    if (req.user && req.user.role === 'shipper') {
+        next();
+    } else {
+        res.status(401).send({ message: 'Invalid Admin Token' });
+    }
 }
 
 module.exports = { getToken, isAuth, isAdmin, isShipper };
